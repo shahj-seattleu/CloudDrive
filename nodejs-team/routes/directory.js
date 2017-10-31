9c0a112fa5baaf6f435c79e0075e1d8f51fd19b6
kanav.gupta <kanavkartik@gmail.com>
2017-10-30 17:13:37 -0700

    Testing with file upload

--- nodejs-team/routes/directory.js
+++ nodejs-team/routes/directory.js
@@ -5,18 +5,46 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 var directories = require('../models/Directory-fs');
+var multer = require('multer');
 
 const log   = require('debug')('nodejs-team:router-directory');
 const error = require('debug')('nodejs-team:error');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "public/uploads");
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+var upload = multer({ storage: storage });
+
+const fs = require("fs");
+
+let filepath = "/Users/kanav/Desktop/CloudDrive";
+
+fs.lstat(filepath, (err, stats) => {
+
+    if(err)
+        return console.log(err); //Handle error
+
+    console.log(`Is file: ${stats.isFile()}`);
+    console.log(`Is directory: ${stats.isDirectory()}`);
+    console.log(`Is symbolic link: ${stats.isSymbolicLink()}`);
+    console.log(`Is FIFO: ${stats.isFIFO()}`);
+    console.log(`Is socket: ${stats.isSocket()}`);
+    console.log(`Is character device: ${stats.isCharacterDevice()}`);
+    console.log(`Is block device: ${stats.isBlockDevice()}`);
+});
+
 
 // Add Directory(File or Folder). (create)
-router.post('/add', (req, res, next) => {
-  console.log(req);
-  //  log('File info='+ util.inspect(req.files));
+router.post( '/add', upload.any(), function( req, res, next ) {
+  // Metadata about the uploaded file can now be found in req.file
 
-
-
+  console.log(req.files);
 });
 
 
