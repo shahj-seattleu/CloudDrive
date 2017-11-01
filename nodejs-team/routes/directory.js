87527b08e66930fa59af2bac59ffed8c4da63998
kanav.gupta <kanavkartik@gmail.com>
2017-10-31 17:12:05 -0700

    Add a static file and folder structure to test

--- nodejs-team/routes/directory.js
+++ nodejs-team/routes/directory.js
@@ -9,43 +9,32 @@ var multer = require('multer');
 
 const log   = require('debug')('nodejs-team:router-directory');
 const error = require('debug')('nodejs-team:error');
-
-var storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, "public/uploads");
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-    }
-});
-
-var upload = multer({ storage: storage });
-
 const fs = require("fs");
-
-let filepath = "/Users/kanav/Desktop/CloudDrive";
-
-fs.lstat(filepath, (err, stats) => {
-
-    if(err)
-        return console.log(err); //Handle error
-
-    console.log(`Is file: ${stats.isFile()}`);
-    console.log(`Is directory: ${stats.isDirectory()}`);
-    console.log(`Is symbolic link: ${stats.isSymbolicLink()}`);
-    console.log(`Is FIFO: ${stats.isFIFO()}`);
-    console.log(`Is socket: ${stats.isSocket()}`);
-    console.log(`Is character device: ${stats.isCharacterDevice()}`);
-    console.log(`Is block device: ${stats.isBlockDevice()}`);
-});
 
 
 // Add Directory(File or Folder). (create)
-router.post( '/add', upload.any(), function( req, res, next ) {
-  // Metadata about the uploaded file can now be found in req.file
+router.post('/add', function(req, res, next) {
+  console.log(`req: ${req.body}`);
+  var filePath = path.join(__dirname,"drive",'Icon.png');
+  console.log(`filePath: ${filePath}`);
+  fs.stat(filePath,(err,fileInfo) => {
+    if (err){
+      next(err);
+      return;
+    }
+    if(fileInfo.isFile()){
+      console.log(`Is file: ${fileInfo.isFile()}`);
+      res.sendFile(filePath);
+    }else if(fileInfo.isDirectory()){
+      console.log(`Is directory: ${fileInfo.isDirectory()}`);
+      res.send('respond with a resource');
+    }else{
+      next();
+    }
 
-  console.log(req.files);
+  });
 });
+
 
 
 module.exports = router;
