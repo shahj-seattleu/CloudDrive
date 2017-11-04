baf7f68c3174201d215812fb288c0acc7988f504
kanav.gupta <kanavkartik@gmail.com>
2017-11-03 17:21:50 -0700

    File system removed

--- nodejs-team/models/index.js
+++ nodejs-team/models/index.js
@@ -17,7 +17,6 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    console.log(`File : ${file}`);
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
