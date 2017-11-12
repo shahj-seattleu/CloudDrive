deb971d31b39b56d7dd36b8fb041aa05f4a69f8b
kanav.gupta <kanavkartik@gmail.com>
2017-11-11 20:47:53 -0800

    updating add files router method

--- nodejs-team/models/drive.js
+++ nodejs-team/models/drive.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       associate: function(models) {
         // associations can be defined here
         Drive.hasMany(models.Drive);
-        Drive.belongsTo(models.Drive, { as: "parent_id" });
+        Drive.belongsTo(models.Drive, { as: "parent" });
 
       }
     }
