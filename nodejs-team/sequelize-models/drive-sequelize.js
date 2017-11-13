4c14f7b5ca14335daf8abaf35ab497adb4e13f50
bhavenya <jayakuma@seattleu.edu>
2017-11-13 00:22:17 -0800

    Changes to move. Implemented cascade move. Need to test nested Folder move.

--- nodejs-team/sequelize-models/drive-sequelize.js
+++ nodejs-team/sequelize-models/drive-sequelize.js
@@ -121,66 +121,97 @@ exports.delete = function (id) {
 });
 }
 
-exports.move = function (sourceId, destParentId) {
-    console.log(`sourceId:${sourceId}  parentId:${destParentId}`)
+exports.move = function (sourceId, destPath) {
+    console.log(`sourceId:${sourceId}  destPath:${destPath}`)
 
     return new Promise((resolve, reject) => {
 
         var destRootPath = path.join(__dirname, '../public/cloud/');
         var destFullPath;
         var destFileName;
+        var isFile;
 
         // Find the name of the file/folder that we're moving
         models.Drive.findById(sourceId).then(function (sourceRow) {
-            if (sourceRow) {
+            if (sourceRow)
+            {
                 destFileName = sourceRow.name;
+                isFile = sourceRow.fileType;
             }
-            else {
+            else
+            {
                 var msg = `Failed to find "name" data for sourceId:${sourceId}`;
                 console.log(msg);
                 reject(msg);
             }
 
-            // Find the path of the destination folder
-            models.Drive.findById(destParentId).then(function (destRow) {
-                if (destRow) {
-                    // Build the path from the parent row
-                    destFullPath = path.join(destRow.path, destFileName);
+            // Use the destination path provided in the function
+                if (destPath)
+                {
+                    // Build the path from the filename and destination path
+                    if(isFile == 1)
+                    {
+                      destFullPath = path.join(destPath, destFileName,'\\');
+                    }
+                    else if(isFile == 2)
+                    {
+                      destFullPath = path.join(destPath, destFileName);
+                    }
                 }
-                else if (destParentId == 0) {
-                    // Use the default root path
-                    destFullPath = path.join(destRootPath, destFileName)
-                }
-                else {
-                    var msg = `Failed to find "path" data for id (destParentId):${destParentId}`;
+                else
+                {
+                    var msg = `Path not valid (destPath):${destPath}`;
                     console.log(msg);
                     reject(msg);
                 }
 
-                console.log(`destParentId full path: '${destFullPath}'`);
+                console.log(`destFullPath full path: '${destFullPath}'`);
 
-                // Update the database with the new path for the sourceId
-                models.Drive.update(
+                // Find new parent ID
+                var destParentId;
+                models.Drive.find({where: {path: destPath }}).then(function (sourceRow) {
+                    if (sourceRow)
                     {
-                        parent_id: destParentId,
-                        path: destPath
-                    },
+                        destParentId = sourceRow.id;
+                    }
+                    else if (destRootPath == destPath)
                     {
-                        where: { id: sourceId }
+                        destParentId = 0;
                     }
-                ).then(function (moved) {
-                    if (moved != 0) {
-                        var msg = `Moved id:${sourceId} to new parent:${destParentId}`;
-                        console.log(msg);
-                        resolve(msg);
-                    }
-                    else {
-                        var msg = `Failed to move id:${sourceId} to new parent:${destParentId}`;
+                    else
+                    {
+                        var msg = `Failed to find "Id" data for destPath:${destPath}`;
                         console.log(msg);
                         reject(msg);
                     }
+
+                    //Update the database with the new path for the sourceId
+                      models.Drive.update({
+                              parent_id: destParentId,
+                              path: destFullPath
+                          },{
+                              where: { id: sourceId }
+                          })
+                          .then(function (moved) {
+                          if (moved != 0) {
+                              var msg = `Moved id:${sourceId} to new parent:${destParentId}`;
+                              console.log(msg);
+                              resolve(msg);
+                          }
+                          else {
+                              var msg = `Failed to move id:${sourceId} to new parent:${destParentId}`;
+                              console.log(msg);
+                              reject(msg);
+                          }
+                        });
+                    if(isFile == 1)
+                    {
+                      //to be tested with nested folders
+                      models.Drive.find({ where: { parent_id: sourceId }}).then(function(drive)  {
+                      exports.move(drive.id,path.join(destFullPath))
+                    });
+                    }
                 });
-            });
-        });
-    });
+});
+});
 };
