73c9140e2f0144f4244e3de046aa4d26d75c2644
kanav.gupta <kanavkartik@gmail.com>
2017-11-19 14:20:00 -0800

    Post script for adding files

--- angular-team/src/app/services/data.service.ts
+++ angular-team/src/app/services/data.service.ts
@@ -1,67 +1,76 @@
 'use strict';
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import {Headers, RequestOptions} from '@angular/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class DataService {
 
-    constructor(public http: Http) {
-        console.log('Data Service Connected ...');
+  constructor(public http: Http) {
+    console.log('Data Service Connected ...');
 
-    };
+  };
 
-    private url: any = {
-        postFileUrl: "someAPI",
-        postFolderUrl: "someAPI",
-        getFileUrl: "someAPI",
-        getFolderURL: "someApi",
-        getFilesUrl: "someAPI",
-    }
-    //Has file data for upload to server
-    postFile(data) {
-        console.log("post file data:" + data);
-        
-        console.log(data);
-        return this.http.post(this.url.postFileUrl, data).map((res: any) => res.json());
+  private url: any = {
+    postFileUrl: "127.0.0.1:3000/files/add",
+    postFolderUrl: "someAPI",
+    getFileUrl: "127.0.0.1:3000/files/list",
+    getFolderURL: "someApi",
+    getFilesUrl: "someAPI",
+  }
 
-    }
-    //Has folder data for upload to server
-    postFolder(data) {
-        console.log("post folder data: " + data);
-        
-        return this.http.post(this.url.postFolderUrl, data).map((res: any) => res.json());
-    }
-    //get folder from server
-    //needs to get folder by id
-    getFolder(id: Number) {
-        const url = '${this.getFolderUrl}/${1d}';
-        return this.http.get(url)
-            .map(res => res.json());
-    }
+  // postFile(data: any) {
+  //   console.log("post file data:" + data);
+  //   const body = { file_path: data, path_id: 0 };
+  //   this.http.post('127.0.0.1:3000/files/add', body, {
+  //     headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
+  //   }).subscribe();
+  //
+  // }
+  //Has file data for upload to server
+  postFile(data) {
+    console.log("post file data:" + data);
 
-    //get a file from server
-    //needs to get file by id
-    getFile(id: Number) {
-        const url = '${this.getFileUrl}/${1d}';
-            return this.http.get(url)
-                .map(res => res.json());
-        }
-    //get files from server
-    //should map to /list
-    getFiles() {
-            return this.http.get(this.url.getFilesUrl)
-                .map(res => res.json());
-        }
+    console.log(data);
+    return this.http.post(this.url.postFileUrl, data).map((res: any) => res.json());
 
-    deleteFile(id: Number){
-        const url = '${this.getFileUrl}/${1d}';
-        return this.http.delete(url)
-        .map(res => res.json());
-    }
-        
+  }
+  //Has folder data for upload to server
+  postFolder(data) {
+    console.log("post folder data: " + data);
+
+    return this.http.post(this.url.postFolderUrl, data).map((res: any) => res.json());
+  }
+  //get folder from server
+  //needs to get folder by id
+  getFolder(id: Number) {
+    const url = '${this.getFolderUrl}/${1d}';
+    return this.http.get(url)
+      .map(res => res.json());
+  }
+
+  //get a file from server
+  //needs to get file by id
+  getFile(id: Number) {
+    const url = '${this.getFileUrl}/${1d}';
+    return this.http.get(url)
+      .map(res => res.json());
+  }
+  //get files from server
+  //should map to /list
+  getFiles() {
+    return this.http.get(this.url.getFilesUrl)
+      .map(res => res.json());
+  }
+
+  deleteFile(id: Number) {
+    const url = '${this.getFileUrl}/${1d}';
+    return this.http.delete(url)
+      .map(res => res.json());
+  }
+
 
 }
--- nodejs-team/routes/drive-route.js
+++ nodejs-team/routes/drive-route.js
@@ -164,7 +164,6 @@ router.post('/move', function(req, res, next) {
 
 });
 
-
 router.post('/add', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-  With, Content-Type, Accept");
