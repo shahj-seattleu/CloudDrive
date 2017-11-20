f66f02b8dd0b8aa58333a2675a28fd53df501204
bhavenya <jayakuma@seattleu.edu>
2017-11-19 23:13:23 -0800

    Testcase for RESPONSE check

--- nodejs-team/test/model-test.js
+++ nodejs-team/test/model-test.js
@@ -1,9 +1,16 @@
-var expect  = require('chai').expect;
-var request = require('request');
+var express = require('express');
 var models = require('../models/index');
 var Drive = require('../models/drive');
+var routes = require("../routes");
 var sequelize = require('sequelize');
 var assert = require('chai').assert;
+var request = require('chai').request;
+var expect  = require('chai').expect;
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
 
 // test case to insert and delete a record into database
 
@@ -15,18 +22,39 @@ var assert = require('chai').assert;
        models.Drive.count({where: { path : 'testpath' }})
         .then(function(count) {
             assert.isAbove(count,-1);
-              console.log("Test Passed");
+              console.log("Test insert Passed");
     });
     });
 
-    it('should insert record into model', function() {
+    it('should delete record from model', function() {
       models.Drive.destroy(
        {where: {path:'testpath'}});
        models.Drive.count({where: { path : 'testpath' }})
         .then(function(count) {
             assert.isAbove(count,-1);
-              console.log("Test Passed");
+              console.log("Test delete Passed");
     });
     });
 
 });
+
+
+describe('/list ', () => {
+      it('it should LIST all the files',function() {
+        console.log("gopal");
+        chai.request('http://127.0.0.1:3000/files/')
+            .get('/list')
+            .end((err, res) => {
+                res.should.be.json;
+                res.should.not.be.html;
+                res.should.not.be.text;
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                console.log("Test response Passed");
+
+            }).catch(err => {
+              reject(`Error while find a parent Drive model`);
+            });
+          });
+      });
